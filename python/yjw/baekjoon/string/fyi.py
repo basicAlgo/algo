42cb6bf4b2943c46b2646956b2b71f5b9e1949ef
SecretFr <linkclean@naver.com>
2021-09-29 23:04:28 +0900

    fyi

---
+++ python/yjw/baekjoon/string/fyi.py
@@ -0,0 +1,14 @@
+'''
+문자열
+https://www.acmicpc.net/problem/17863
+'''
+
+li = input()
+a = int(li[0])
+b = int(li[1])
+c = int(li[2])
+
+if a == 5 and b == 5 and c == 5:
+    print("YES")
+else:
+    print("NO")
